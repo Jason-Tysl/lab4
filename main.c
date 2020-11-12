c02175cf2f2d1069836c66217b8ff62107b96511
Jason Tysl <47398566+Jason-Tysl@users.noreply.github.com>
2020-11-11 23:24:12 -0500

    first attempt at threading

--- main.c
+++ main.c
@@ -2,33 +2,53 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
-#define n 12
-#define m 10
-#define p 5
+#define N 12
+#define M 10
+#define P 5
+#define MAX_THREADS 2
 
-void initializeMatrices(int a[n][m], int b[m][p]) {
+int threadNumber;
+
+int a[N][M];
+int b[M][P];
+int c[N][P];
+
+
+void initializeMatrices() {
     int i, j;
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < m; j++) {
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; j++) {
             a[i][j] = i + j;
         }
     }
-    for (i = 0; i < m; i++) {
-        for (j = 0; j < p; j++) {
+    for (i = 0; i < M; i++) {
+        for (j = 0; j < P; j++) {
             b[i][j] = j;
         }
     }
 }
 
-void givenMatMult (int a[n][m], int b[m][p], int c[n][p]) {
+void givenMatMult () {
     // int rowsOfA = sizeof(a) / sizeof(a[0]);             /* a is nxm -- rows = n */
     // int columnsOfA = sizeof(a[0]) / sizeof(a[0][0]);    /* columnsOfA = rowsOfB = m */
     // int columnsOfB = sizeof(b[0]) / sizeof(b[0][0]);    /* b is mxp -- columns = p */
     int i, j, k;
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < p; j++) {
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < P; j++) {
             c[i][j] = 0;
-            for (k = 0; k < m; k++) {
+            for (k = 0; k < M; k++) {
+                c[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+}
+
+void *multiMultip(void *param) {
+    int i, j, k;
+    int thNum = threadNumber++;
+    for (i = thNum * N/ MAX_THREADS; i < (thNum +1) * N / MAX_THREADS; i++) {
+        for (j = 0; j < P; j++) {
+            for (k = 0; k < M; k++) {
                 c[i][j] += a[i][k] * b[k][j];
             }
         }
@@ -36,18 +56,43 @@ void givenMatMult (int a[n][m], int b[m][p], int c[n][p]) {
 }
 
 int main(void) {
-    int a[n][m];
-    int b[m][p];
-    int c[n][p];
     int i, j;
-    initializeMatrices(a, b);
-    
-    givenMatMult(a, b, c);
 
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < p; j++) {
+    pthread_attr_t attr;
+    pthread_t tid[MAX_THREADS];
+    pthread_attr_init(&attr);
+
+    initializeMatrices();
+
+    // givenMatMult();
+
+    threadNumber = 0;
+    for (i = 0; i < MAX_THREADS; i++) {
+        if (pthread_create(&(tid[i]), &attr, multiMultip, NULL)) {
+            printf("Failed to create thread %d.\n", i);
+        } else {
+            printf("Thread %d created successfully.\n", i);
+        }
+    }
+
+    for (i = 0; i < MAX_THREADS; i++) {
+        if (pthread_cancel(tid[i])) {
+            printf("Failed to cancel thread %d.\n", i);
+        } else {
+            printf("Thread %d cancelled successfully.\n", i);
+        }
+        if (pthread_join(tid[i], NULL)) {
+            printf("Failed to join thread %d.\n", i);
+        } else {
+            printf("Thread %d joined successfully.\n", i);
+        }
+    }
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < P; j++) {
             printf("%3d ", c[i][j]);
         }
         printf("\n");
     }
+    return(0);
 }
